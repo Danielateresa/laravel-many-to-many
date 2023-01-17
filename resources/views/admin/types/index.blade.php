b8393d781bcab7f45db139f9519cafeed935d6a2
Danielateresa <dany.t_87@hotmail.it>
2023-01-17 18:54:35 +0100

    Add delete method, fixed create

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -54,26 +54,26 @@
                             <td>
                                 <!-- Modal trigger button -->
                                 <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                    data-bs-target="#deleteProject-">
+                                    data-bs-target="#deleteType-{{$type->slug}}">
                                     <i class="fa-solid fa-trash text-white"></i>
                                 </button>
 
                                 <!-- Modal Body -->
                                 <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-                                <div class="modal fade" id="deleteProject-" tabindex="-1" data-bs-backdrop="static"
-                                    data-bs-keyboard="false" role="dialog" aria-labelledby="modalProjectId-"
-                                    aria-hidden="true">
+                                <div class="modal fade" id="deleteType-{{$type->slug}}" tabindex="-1"
+                                    data-bs-backdrop="static" data-bs-keyboard="false" role="dialog"
+                                    aria-labelledby="modalTypeId-{{$type->slug}}" aria-hidden="true">
                                     <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
                                         role="document">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h5 class="modal-title" id="modalProjectId-">Delete
-                                                    Project</h5>
+                                                <h5 class="modal-title" id="modaltypeId-{{$type->slug}}">Delete
+                                                    type</h5>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                Do you want to delete this project permanentely?
+                                                Do you want to delete {{$type->name}} project permanentely?
                                             </div>
                                             <div class="modal-footer">
                                                 <button type="button" class="btn btn-secondary"
