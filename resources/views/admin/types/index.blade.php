496a410981153a99f9909dda7a3c2864804760f6
Danielateresa <dany.t_87@hotmail.it>
2023-01-17 17:25:39 +0100

    Add table for model type crud on types index

---
+++ resources/views/admin/types/index.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="top_content d-flex">
+    <h1 class="py-3">Projects</h1>
+    <a href="{{route('admin.projects.create')}}" class="btn btn-primary ms-auto align-self-center">Add Project</a>
+</div>
+
+@if(session('message'))
+<div class="alert alert-success alert-dismissible fade show" role="alert">
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
+    <strong>{{session('message')}}</strong>
+</div>
+@endif
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+            <div class="table-responsive">
+                <table class="table table-striped
+    table-hover	
+    table-borderless
+    table-primary
+    align-middle">
+                    <thead class="table-light">
+                        <tr>
+                            <th>id</th>
+                            <th>name</th>
+                            <th>slug</th>
+                            <th>project count</th>
+                            <th>actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        @forelse($types as $type)
+                        <tr class="table-primary">
+                            <td scope="row">{{$type->id}}</td>
+                            <td>{{$type->name}}</td>
+                            <td>{{$type->slug}}</td>
+                            <td>-</td>
+                            <td>
+                                <!-- Modal trigger button -->
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                    data-bs-target="#deleteProject-">
+                                    <i class="fa-solid fa-trash text-white"></i>
+                                </button>
+
+                                <!-- Modal Body -->
+                                <!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
+                                <div class="modal fade" id="deleteProject-" tabindex="-1" data-bs-backdrop="static"
+                                    data-bs-keyboard="false" role="dialog" aria-labelledby="modalProjectId-"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm"
+                                        role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="modalProjectId-">Delete
+                                                    Project</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Do you want to delete this project permanentely?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+
+                                                <form class="d-inline"
+                                                    action="{{route('admin.types.destroy', $type->slug)}}"
+                                                    method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        @empty
+                        <h3>No project types on database yet</h3>
+                        @endforelse
+                    </tbody>
+                    <tfoot>
+
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\Admin\TypeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +27,7 @@ Route::middleware('auth', 'verified')->name('admin.')->prefix('admin')->group(fu
         'projects'=> 'project:slug'
     ]);
 
-    Route::resource('types', ProjectController::class)->parameters([
+    Route::resource('types', typeController::class)->parameters([
         'types'=> 'type:slug'])->except(['show','create','edit',]); //creo le view eccetto show, create e edit, poichè non mi servono
 });
 
